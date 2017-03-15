a8103a95d4802cf6e3355a27f43d440ba6b8a517
jfmolano1587 <jose.francisco.molano@gmail.com>
2017-03-15 21:49:41 +0000

    bind ip

---
+++ serial_raspberry.py
@@ -0,0 +1,7 @@
+import serial
+
+with serial.Serial('/dev/rfcomm0', 9600, timeout=10) as ser:
+	x = ser.read()          # read one byte
+	s = ser.read(10)        # read up to ten bytes (timeout)
+	line = ser.readline()   # read a '\n' terminated line
+	print line
