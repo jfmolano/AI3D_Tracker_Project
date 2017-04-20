b162e7cbec76b4be5e603ea291095351fc8805da
jfmolano1587 <jose.francisco.molano@gmail.com>
2017-04-20 00:12:50 +0000

    center discovery

--- Raspberry_IR_Camera/stream_2_cv.py
+++ Raspberry_IR_Camera/stream_2_cv.py
@@ -2,6 +2,7 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import imutils
 import cv2
  
 # initialize the camera and grab a reference to the raw camera capture
@@ -20,10 +21,30 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	image = frame.array
 
 	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	ret,thresh1 = cv2.threshold(gray_image,200,255,cv2.THRESH_BINARY)
+	ret,thresh1 = cv2.threshold(gray_image,127,255,cv2.THRESH_BINARY)
+
+	cnts = cv2.findContours(thresh1.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+	for c in cnts:
+		# compute the center of the contour
+		M = cv2.moments(c)
+		try:
+                	cX = int(M["m10"] / M["m00"])
+                	cY = int(M["m01"] / M["m00"])
+		except:
+                	cX = 0
+                	cY = 0
+	 
+		# draw the contour and center of the shape on the image
+		cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+		cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+		cv2.putText(image, "center", (cX - 20, cY - 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
  
 	# show the frame
-	cv2.imshow("Frame", thresh1)
+	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
  
 	# clear the stream in preparation for the next frame
