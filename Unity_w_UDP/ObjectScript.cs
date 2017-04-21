c72ed5ee814c466d4f422bd938de3b9d2d024584
José Francisco Molano <Jos Francisco Molano>
2017-04-21 14:18:10 -0500

    Unity w UPD

---
+++ Unity_w_UDP/ObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+GameObject sphere;
+	// Use this for initialization
+	void Start () {
+		//sphere = new GameObject.Find("/Sphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find("Sphere").transform.position = new Vector3(1, 1, 1);
+	}
+}
